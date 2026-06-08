6c21bb73cc127cb32039aa42d43ea26592395200
recebadavi47 <recebadavi47@gmail.com>
2026-06-08 21:23:29 +0000

    ULTIMAVERSAO

--- React_vazio/vazio/App.js
+++ React_vazio/vazio/App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground 
+      source={{ uri: 'https://i.ytimg.com/vi/L9Km9DLDd7E/maxresdefault.jpg' }}
+      style={styles.background}
+    >
+      <ScrollView>
+        <View style={styles.titulo}>
+          <Text style={{ 
+            fontWeight: '900',
+            color: '#ebebeb',
+            fontSize: 22
+        }}>
+            SKINNY LOOSER - SUB URBAN
+          </Text>
+        </View>
+        
+        <View style={styles.cont_texto}>
+          <Text style={{ 
+            fontWeight: '900',
+            color: '#4690ff',
+            fontSize: 22
+        }}>ABC</Text>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={{
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 2,
+              width: 200,
+              padding: 10,
+              backgroundColor: 'white', 
+            }}
+            placeholder="Digite seu texto"
+          />
+        </View>
+      </ScrollView>
+    </ImageBackground>  
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
+    height: '100%',
+  },
+  titulo: {
+    backgroundColor: 'rgba(255, 155, 217, 0.8)',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    margin: 10,
+    borderRadius: 10,
+    fontWeight: 'bold',
   },
+  cont_texto: {
+    backgroundColor: 'rgba(255, 155, 217, 0.8)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    margin: 10,
+    borderRadius: 10,
+    color: 'white',
+  },
+  inputContainer: {
+    padding: 20,
+    alignItems: 'center',
+  }
 });
